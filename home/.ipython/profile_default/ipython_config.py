76630e1c6c5b30ae45a8697559af783a136bed07
Chris Bouchard <chris@upliftinglemma.net>
2018-08-05 14:37:48 -0400

    Set IPython to use VI mode

--- home/.ipython/profile_default/ipython_config.py
+++ home/.ipython/profile_default/ipython_config.py
@@ -1,12 +1,7 @@
-from IPython import get_ipython
-from prompt_toolkit.filters import ViInsertMode
-
 from powerline.bindings.ipython.since_5 import PowerlinePrompts
 
-ip = get_ipython()
-insert_mode = ViInsertMode()
-
 c = get_config()
-c.TerminalInteractiveShell.simple_prompt = False
+c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
+c.TerminalInteractiveShell.simple_prompt = False
 
