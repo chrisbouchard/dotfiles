5642a0a0b25b5f13053ee414a5476ecca1b15610
Chris Bouchard <chris@upliftinglemma.net>
2018-08-05 14:33:38 -0400

    Use the default IPython profile

--- home/.ipython/profile_default/ipython_config.py
+++ home/.ipython/profile_default/ipython_config.py
@@ -1,4 +1,10 @@
+from IPython import get_ipython
+from prompt_toolkit.filters import ViInsertMode
+
 from powerline.bindings.ipython.since_5 import PowerlinePrompts
+
+ip = get_ipython()
+insert_mode = ViInsertMode()
 
 c = get_config()
 c.TerminalInteractiveShell.simple_prompt = False
