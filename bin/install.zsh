876a3b3c352efe88dffff7baf028ba1dab68a62c
Chris Bouchard <chris@upliftinglemma.net>
2019-11-02 15:08:20 -0400

    Refactor install script and add notify-send.sh

--- bin/install.zsh
+++ bin/install.zsh
@@ -1,20 +1,60 @@
 #!/bin/env zsh
 
-# Install Homeshick (Shell-based Homesick replacement).
-REPO_DIR=$HOME/.homesick/repos
-mkdir -p $REPO_DIR
-git clone https://github.com/andsens/homeshick.git $REPO_DIR/homeshick
-source $REPO_DIR/homeshick/homeshick.sh
+# Directory where Homeshick will install its repositories
+repo_dir=$HOME/.homesick/repos
 
-# Clone my dotfiles.
-homeshick clone --batch chrisbouchard/dotfiles
+# Names of repositories we want to install
+repo_names=(
+    # My dotfiles
+    chrisbouchard/dotfiles
 
-# Clone other utilities that my dotfiles use (via symlink).
-homeshick clone --batch robbyrussell/oh-my-zsh
-homeshick clone --batch Shougo/dein.vim
-homeshick clone --batch Tarrasch/zsh-bd
-homeshick clone --batch tmux-plugins/tpm
-homeshick clone --batch zsh-users/zsh-syntax-highlighting
+    # Other utilities that my dotfiles use (via symlink)
+    robbyrussell/oh-my-zsh
+    Shougo/dein.vim
+    Tarrasch/zsh-bd
+    tmux-plugins/tpm
+    vlevit/notify-send.sh
+    zsh-users/zsh-syntax-highlighting
+)
+
+
+# Function to print a colored message at each step.
+echo_step() {
+    local bold=$(tput bold)
+    local green=$(tput setaf 2)
+    local white=$(tput setaf 7)
+    local reset=$(tput sgr0)
+
+    echo "$bold$green==> $white$1...$reset"
+}
+
+
+echo_step 'Installing Homeshick'
+
+# Install Homeshick (Shell-based Homesick replacement). If it's already
+# installed we'll skip this step. We assume it's installed if $repo_dir already
+# exists.
+if [[ ! -d $repo_dir ]]
+then
+    mkdir -p $repo_dir
+    git clone https://github.com/andsens/homeshick.git $repo_dir/homeshick
+else
+    echo "    Repository directory $repo_dir already exists, skipping installation."
+fi
+
+source $repo_dir/homeshick/homeshick.sh
+
+echo
+echo_step 'Installing Repositories'
+
+# Clone each repository
+for repo_name in $repo_names
+do
+    homeshick clone --batch $repo_name
+done
+
+echo
+echo_step 'Linking'
 
 # Install dotfiles via Homeshick.
 homeshick link --force
