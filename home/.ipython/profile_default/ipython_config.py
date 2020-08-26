3579f02374ed56841384ab044666c54abf33aabd
Chris Bouchard <chris@upliftinglemma.net>
2020-08-25 21:39:44 -0400

    Turn off Powerline for IPython

--- home/.ipython/profile_default/ipython_config.py
+++ home/.ipython/profile_default/ipython_config.py
@@ -1,7 +1,7 @@
-from powerline.bindings.ipython.since_5 import PowerlinePrompts
-
-c = get_config()
-c.TerminalInteractiveShell.editing_mode = 'vi'
-c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
-c.TerminalInteractiveShell.simple_prompt = False
-
+# TODO: Uncomment once we have Powerline 2.8
+# from powerline.bindings.ipython.since_7 import PowerlinePrompts
+#
+# c = get_config()
+# c.TerminalInteractiveShell.editing_mode = 'vi'
+# c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
+# c.TerminalInteractiveShell.simple_prompt = False
