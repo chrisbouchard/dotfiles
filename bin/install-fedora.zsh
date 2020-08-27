ecd3b814cc84186105ca7a836b63d55b7f04a71e
Chris Bouchard <chris@upliftinglemma.net>
2020-08-26 22:56:30 -0400

    Clean up install script and add Fedora-specific script

---
+++ bin/install-fedora.zsh
@@ -0,0 +1,109 @@
+#!/bin/env zsh
+
+fedora_version=$(rpm -E %fedora)
+
+dnf_repos=(
+    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$fedora_version.noarch.rpm
+    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$fedora_version.noarch.rpm
+)
+
+dnf_coprs=(
+    agriffis/neovim-nightly
+    jdoss/wireguard
+)
+
+dnf_packages=(
+    autossh
+    chrome-gnome-shell
+    dropbox
+    figlet
+    fzf
+    httpie
+    ImageMagick
+    java-1.8.0-openjdk
+    java-11-openjdk
+    neovim
+    podman
+    podman-docker
+    postgresql
+    postgresql-contrib
+    postgresql-server
+    texlive
+    thefuck
+    tmux
+    wine
+    wireguard-dkms
+    wireguard-tools
+)
+
+flatpak_packages=(
+    com.discordapp.Discord
+    com.googleplaymusicdesktopplayer.GPMDP
+    com.mojang.Minecraft
+    com.valvesoftware.Steam
+    org.freedesktop.Platform.ffmpeg
+    org.gimp.GIMP
+    org.inkscape.Inkscape
+    org.keepassxc.KeePassXC
+    org.mozilla.FirefoxDevEdition
+    org.signal.Signal
+    org.videolan.VLC
+)
+
+
+# Function to print a colored message at the start of each step.
+step_start() {
+    local bold=$(tput bold)
+    local green=$(tput setaf 2)
+    local white=$(tput setaf 7)
+    local reset=$(tput sgr0)
+
+    echo "${bold}${green}>>> ${white}$1...${reset}"
+}
+
+# Function to print a colored message at the end of each step.
+step_end() {
+    local bold=$(tput bold)
+    local green=$(tput setaf 2)
+    local white=$(tput setaf 7)
+    local reset=$(tput sgr0)
+
+    echo "${bold}${green}>>> ${white}Done${reset}"
+    echo
+}
+
+
+step_start 'Adding required DNF repositories'
+
+    sudo dnf install -y $dnf_repos
+    
+    for dnf_copr in $dnf_coprs
+    do
+        sudo dnf copr enable -y $dnf_copr
+    done
+
+step_end
+
+
+step_start 'Installing packages from DNF'
+
+    sudo dnf install -y $dnf_packages
+
+step_end
+
+
+step_start 'Adding required Flatpak repositories'
+
+    # TODO: Move this to an array?
+    flatpak remote-add --from org.mozilla.FirefoxRepo \
+        https://firefox-flatpak.mojefedora.cz/org.mozilla.FirefoxRepo.flatpakrepo
+
+step_end
+
+
+step_start 'Installing packages from Flatpak'
+
+    flatpak install -y $flatpak_packages
+
+step_end
+
--- bin/install.zsh
+++ bin/install.zsh
@@ -21,6 +21,14 @@ repo_names=(
     chrisbouchard/dotfiles
 )
 
+pip_packages=(
+    powerline-gitstatus
+    powerline-pyenv
+    powerline-status
+    pyls
+    pynvim
+)
+
 
 # Function to print a colored message at the start of each step.
 step_start() {
@@ -43,45 +51,80 @@ step_end() {
     echo
 }
 
+# Function to check if a command exists.
+command_exists() {
+    type $1 &>/dev/null
+}
+
 
 step_start 'Installing Homeshick'
 
-# Install Homeshick (Shell-based Homesick replacement). If it's already
-# installed we'll skip this step. We assume it's installed if $repo_dir already
-# exists.
-if [[ ! -d $repo_dir ]]
-then
-    mkdir -p $repo_dir
-    git clone https://github.com/andsens/homeshick.git $repo_dir/homeshick
-else
-    echo -e "\tRepository directory $repo_dir already exists, skipping installation."
-fi
+    # Install Homeshick (Shell-based Homesick replacement). If it's already
+    # installed we'll skip this step. We assume it's installed if $repo_dir already
+    # exists.
+    if [[ ! -d $repo_dir ]]
+    then
+        mkdir -p $repo_dir
+        git clone https://github.com/andsens/homeshick.git $repo_dir/homeshick
+    else
+        echo -e "\tRepository directory $repo_dir already exists, skipping installation."
+    fi
 
-source $repo_dir/homeshick/homeshick.sh
+    source $repo_dir/homeshick/homeshick.sh
 
 step_end
+
 
 step_start 'Installing Repositories'
 
-# Clone each repository
-for repo_name in $repo_names
-do
-    homeshick clone --batch $repo_name
-done
+    # Clone each repository
+    for repo_name in $repo_names
+    do
+        homeshick clone --batch $repo_name
+    done
 
 step_end
+
 
 step_start 'Linking'
 
-# Install dotfiles via Homeshick.
-homeshick link --force
+    # Install dotfiles via Homeshick.
+    homeshick link --force
 
 step_end
 
-if [[ -x systemctl ]]
+
+if command_exists systemctl
 then
     step_start 'Reloading systemd configuration'
-    systemctl --user daemon-reload
+
+        systemctl --user daemon-reload
+
+    step_end
+fi
+
+
+if command_exists pip3
+then
+    step_start 'Installing packages from Pip'
+
+        pip3 install --user $pip_packages
+
+    step_end
+fi
+
+
+if ! command_exists rustup
+then
+    step_start 'Installing from Rustup'
+
+        # https://rustup.rs/
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
+            sh -s -- -y --no-modify-path --profile complete
+        # Skip self-update because we just installed it.
+        rustup install --no-self-update nightly
+        rustup component add --toolchain nightly rust-analyzer-preview
+
     step_end
 fi
 
