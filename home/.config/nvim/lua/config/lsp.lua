6dab9e345d149ba205e8b5f51b9c69b1b16a9ced
Chris Bouchard <chris@upliftinglemma.net>
2021-06-23 23:27:08 -0400

    Make the TypeScript LSP server work with Yarn PNP

--- home/.config/nvim/lua/config/lsp.lua
+++ home/.config/nvim/lua/config/lsp.lua
@@ -1,3 +1,5 @@
+local nvim_lsp = require('lspconfig')
+
 local M = {}
 
 -- Server overrides
@@ -11,6 +13,22 @@ M.servers = {
         cmd = { "rustup", "run", "nightly", "rust-analyzer" }
     },
     texlab = {},
+    tsserver = {
+        on_new_config = function (new_config)
+            local pnp_root = nvim_lsp.util.search_ancestors(new_config.root_dir, function (path)
+                local bin = nvim_lsp.util.path.join(path, ".yarn", "sdks", "typescript", "bin", "tsserver")
+                if nvim_lsp.util.path.is_file(bin) then
+                    return bin
+                end
+            end)
+
+            if pnp_root then
+                local pnp_tsserver = nvim_lsp.util.path.join(pnp_root, ".yarn", "sdks", "typescript", "bin", "tsserver")
+                table.insert(new_config.cmd, "--tsserver-path")
+                table.insert(new_config.cmd, pnp_tsserver)
+            end
+        end,
+    },
     vimls = {},
 }
 
