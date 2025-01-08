da3f0aebed1715b5b92f002200d87ced523963ec
Chris Bouchard <cbouchard@seatgeek.com>
2025-01-08 11:59:24 -0500

    Remove ipython config
    I don't actually use ipython. I tried to get it going for a bit, since it
    supported powerline, but I don't use powerline anymore either.

--- home/.ipython/profile_default/ipython_config.py
+++
@@ -1,7 +0,0 @@
-# TODO: Uncomment once we have Powerline 2.8
-# from powerline.bindings.ipython.since_7 import PowerlinePrompts
-#
-# c = get_config()
-# c.TerminalInteractiveShell.editing_mode = 'vi'
-# c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
-# c.TerminalInteractiveShell.simple_prompt = False
